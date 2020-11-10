a962d6c10c6f62f97e6c2e33f14e960a8ceb06ba
Felix Beil <felix.beil@tum.de>
2020-11-10 15:57:59 +0100

    parse: Account for additional column in output files
    Since the queue sice factor has been added as a collumn, further
    processing has to be adjusted. This is a simple increase of the
    column indices. However it doesn't yet generate usefull results.

--- proc-all.sh
+++ proc-all.sh
@@ -3,7 +3,7 @@
 for measure in goodput cwnd_evolution; do
   for pep in none bbr hybla "fec"; do
     outpref=tcp"_"$pep
-    grep $pep tcp_$measure.csv | awk -F, '{ print $1, $2, $3, $5, $6, $7, $8, $9 }' >$outpref"_"$measure.csv
+    grep $pep tcp_$measure.csv | awk -F, '{ print $1, $2, $3, $5, $6, $7, $8, $9, $10 }' >$outpref"_"$measure.csv
   done
 done
 
@@ -11,12 +11,17 @@ for prot in 'tcp' 'tls'; do
   for measure in time_to_first_byte connection_establishment; do
     for pep in none bbr hybla fec; do
       prefix=$prot"_"$pep
-      grep $pep $prot"_"$measure.csv | awk -F, '{ print $1, $2, $3, $5, $6, $7, $8 }' >$prefix"_"$measure.csv
+      grep $pep $prot"_"$measure.csv | awk -F, '{ print $1, $2, $3, $5, $6, $7, $8, $9 }' >$prefix"_"$measure.csv
     done
   done
 done
 
-fileset="tcp_none_connection_establishment tcp_bbr_connection_establishment tcp_hybla_connection_establishment tcp_fec_connection_establishment tls_none_connection_establishment tls_bbr_connection_establishment tls_hybla_connection_establishment tls_fec_connection_establishment quic_connection_establishment quic_fec_connection_establishment tcp_none_time_to_first_byte tcp_bbr_time_to_first_byte tcp_hybla_time_to_first_byte tcp_fec_time_to_first_byte tls_none_time_to_first_byte tls_bbr_time_to_first_byte tls_hybla_time_to_first_byte tls_fec_time_to_first_byte quic_time_to_first_byte quic_fec_time_to_first_byte"
+fileset="tcp_none_connection_establishment tcp_bbr_connection_establishment tcp_hybla_connection_establishment tcp_fec_connection_establishment
+  tls_none_connection_establishment tls_bbr_connection_establishment tls_hybla_connection_establishment tls_fec_connection_establishment
+  quic_connection_establishment quic_fec_connection_establishment
+  tcp_none_time_to_first_byte tcp_bbr_time_to_first_byte tcp_hybla_time_to_first_byte tcp_fec_time_to_first_byte
+  tls_none_time_to_first_byte tls_bbr_time_to_first_byte tls_hybla_time_to_first_byte tls_fec_time_to_first_byte
+  quic_time_to_first_byte quic_fec_time_to_first_byte"
 
 # Plot files with a single data point per file
 
@@ -152,10 +157,12 @@ EOF
 done
 
 # Plot files with a single data point per file
-fileset="tcp_none_goodput tcp_bbr_goodput tcp_hybla_goodput tcp_fec_goodput tcp_none_cwnd_evolution tcp_bbr_cwnd_evolution tcp_hybla_cwnd_evolution tcp_fec_cwnd_evolution quic_goodput quic_fec_goodput quic_cwnd_evolution quic_fec_cwnd_evolution"
+fileset="tcp_none_goodput tcp_bbr_goodput tcp_hybla_goodput tcp_fec_goodput
+  tcp_none_cwnd_evolution tcp_bbr_cwnd_evolution tcp_hybla_cwnd_evolution tcp_fec_cwnd_evolution
+  quic_goodput quic_fec_goodput quic_cwnd_evolution quic_fec_cwnd_evolution"
 
 for file in $fileset; do
-  sed -e /delay/d -e "s/,/ /g" -e "s/ms//" -e "s/mbit//" $file.csv | awk '{ if ($1 != owd) { print (""); owd = $1; } if ($4 < 30) print; }' >$file.dat
+  sed -e /delay/d -e "s/,/ /g" -e "s/ms//" -e "s/mbit//" $file.csv | awk '{ if ($1 != owd) { print (""); owd = $1; } if ($5 < 30) print; }' >$file.dat
 
   case $file in
   tcp_none*goodput*)
@@ -233,18 +240,18 @@ set xrange [0:30]
 set term pdf size 12cm, 6cm
 set output '$file-$sat.pdf'
 set pointsize 1
-plot '$file.dat' every 1::0:$block:29:$block using   (\$4+1):(\$5/1000) with linespoints pt 2 lc "black" title '1 Mbps, p=0.01%', \
-              '' every 1::30:$block:59:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "black" title '1 Mbps, p=0.1%', \
-              '' every 1::60:$block:89:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "black" title '1 Mbps, p=1.0%', \
-              '' every 1::90:$block:119:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "black" title '1 Mbps, p=5.0%', \
-              '' every 1::120:$block:149:$block using (\$4+1):(\$5/1000) with linespoints pt 2 lc "blue" title '10 Mbps, p=0.01%', \
-              '' every 1::150:$block:179:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "blue" title '10 Mbps, p=0.1%', \
-              '' every 1::180:$block:209:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "blue" title '10 Mbps, p=1.0%', \
-              '' every 1::210:$block:239:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "blue" title '10 Mbps, p=5.0%', \
-              '' every 1::240:$block:269:$block using (\$4+1):(\$5/1000) with linespoints pt 2 lc "red" title '100 Mbps, p=0.01%', \
-              '' every 1::270:$block:299:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "red" title '100 Mbps, p=0.1%', \
-              '' every 1::300:$block:329:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "red" title '100 Mbps, p=1.0%', \
-              '' every 1::330:$block:359:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "red" title '100 Mbps, p=5.0%'
+plot '$file.dat' every 1::0:$block:29:$block using   (\$5+1):(\$6/1000) with linespoints pt 2 lc "black" title '1 Mbps, p=0.01%', \
+              '' every 1::30:$block:59:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "black" title '1 Mbps, p=0.1%', \
+              '' every 1::60:$block:89:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "black" title '1 Mbps, p=1.0%', \
+              '' every 1::90:$block:119:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "black" title '1 Mbps, p=5.0%', \
+              '' every 1::120:$block:149:$block using (\$5+1):(\$6/1000) with linespoints pt 2 lc "blue" title '10 Mbps, p=0.01%', \
+              '' every 1::150:$block:179:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "blue" title '10 Mbps, p=0.1%', \
+              '' every 1::180:$block:209:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "blue" title '10 Mbps, p=1.0%', \
+              '' every 1::210:$block:239:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "blue" title '10 Mbps, p=5.0%', \
+              '' every 1::240:$block:269:$block using (\$5+1):(\$6/1000) with linespoints pt 2 lc "red" title '100 Mbps, p=0.01%', \
+              '' every 1::270:$block:299:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "red" title '100 Mbps, p=0.1%', \
+              '' every 1::300:$block:329:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "red" title '100 Mbps, p=1.0%', \
+              '' every 1::330:$block:359:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "red" title '100 Mbps, p=5.0%'
 EOF
 
   done
@@ -308,26 +315,26 @@ set term pdf size 16cm, 9cm
 set output '$file-$sat-$rate.pdf'
 set pointsize 1
 set logscale y
-plot 'tcp_none_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$4+1):(\$5/1000) with linespoints pt 2 lc "black" title 'TCP p=0.01%', \
-              '' every 1::$offset+30:$block:$offset+59:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "black" title 'TCP p=0.1%', \
-              '' every 1::$offset+60:$block:$offset+89:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "black" title 'TCP p=1.0%', \
-              '' every 1::$offset+90:$block:$offset+119:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "black" title 'TCP p=5.0%', \
-     'tcp_bbr_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$4+1):(\$5/1000) with linespoints pt 2 lc "red" title 'TCP PEP (BBR) p=0.01%', \
-              '' every 1::$offset+30:$block:$offset+59:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "red" title 'TCP PEP (BBR) p=0.1%', \
-              '' every 1::$offset+60:$block:$offset+89:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "red" title 'TCP PEP (BBR) p=1.0%', \
-              '' every 1::$offset+90:$block:$offset+119:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "red" title 'TCP PEP (BBR) p=5.0%', \
-     'tcp_fec_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$4+1):(\$5/1000) with linespoints pt 2 lc "dark-violet" title 'TCP (FEC-tunnel) p=0.01%', \
-              '' every 1::$offset+30:$block:$offset+59:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "dark-violet" title 'TCP (FEC-tunnel) p=0.1%', \
-              '' every 1::$offset+60:$block:$offset+89:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "dark-violet" title 'TCP (FEC-tunnel) p=1.0%', \
-              '' every 1::$offset+90:$block:$offset+119:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "dark-violet" title 'TCP (FEC-tunnel) p=5.0%', \
-     'quic_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$4+1):(\$5/1000) with linespoints pt 2 lc "blue" title 'QUIC p=0.01%', \
-              '' every 1::$offset+30:$block:$offset+59:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "blue" title 'QUIC p=0.1%', \
-              '' every 1::$offset+60:$block:$offset+89:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "blue" title 'QUIC p=1.0%', \
-              '' every 1::$offset+90:$block:$offset+119:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "blue" title 'QUIC p=5.0%', \
-     'quic_fec_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$4+1):(\$5/1000) with linespoints pt 2 lc "dark-green" title 'QUIC (FEC-tunnel) p=0.01%', \
-              '' every 1::$offset+30:$block:$offset+59:$block using (\$4+1):(\$5/1000) with linespoints pt 4 lc "dark-green" title 'QUIC (FEC-tunnel) p=0.1%', \
-              '' every 1::$offset+60:$block:$offset+89:$block using (\$4+1):(\$5/1000) with linespoints pt 8 lc "dark-green" title 'QUIC (FEC-tunnel) p=1.0%', \
-              '' every 1::$offset+90:$block:$offset+119:$block using (\$4+1):(\$5/1000) with linespoints pt 10 lc "dark-green" title 'QUIC (FEC-tunnel) p=5.0%'
+plot 'tcp_none_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$5+1):(\$6/1000) with linespoints pt 2 lc "black" title 'TCP p=0.01%', \
+              '' every 1::$offset+30:$block:$offset+59:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "black" title 'TCP p=0.1%', \
+              '' every 1::$offset+60:$block:$offset+89:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "black" title 'TCP p=1.0%', \
+              '' every 1::$offset+90:$block:$offset+119:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "black" title 'TCP p=5.0%', \
+     'tcp_bbr_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$5+1):(\$6/1000) with linespoints pt 2 lc "red" title 'TCP PEP (BBR) p=0.01%', \
+              '' every 1::$offset+30:$block:$offset+59:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "red" title 'TCP PEP (BBR) p=0.1%', \
+              '' every 1::$offset+60:$block:$offset+89:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "red" title 'TCP PEP (BBR) p=1.0%', \
+              '' every 1::$offset+90:$block:$offset+119:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "red" title 'TCP PEP (BBR) p=5.0%', \
+     'tcp_fec_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$5+1):(\$6/1000) with linespoints pt 2 lc "dark-violet" title 'TCP (FEC-tunnel) p=0.01%', \
+              '' every 1::$offset+30:$block:$offset+59:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "dark-violet" title 'TCP (FEC-tunnel) p=0.1%', \
+              '' every 1::$offset+60:$block:$offset+89:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "dark-violet" title 'TCP (FEC-tunnel) p=1.0%', \
+              '' every 1::$offset+90:$block:$offset+119:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "dark-violet" title 'TCP (FEC-tunnel) p=5.0%', \
+     'quic_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$5+1):(\$6/1000) with linespoints pt 2 lc "blue" title 'QUIC p=0.01%', \
+              '' every 1::$offset+30:$block:$offset+59:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "blue" title 'QUIC p=0.1%', \
+              '' every 1::$offset+60:$block:$offset+89:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "blue" title 'QUIC p=1.0%', \
+              '' every 1::$offset+90:$block:$offset+119:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "blue" title 'QUIC p=5.0%', \
+     'quic_fec_$file.dat' every 1::$offset+0:$block:$offset+29:$block using  (\$5+1):(\$6/1000) with linespoints pt 2 lc "dark-green" title 'QUIC (FEC-tunnel) p=0.01%', \
+              '' every 1::$offset+30:$block:$offset+59:$block using (\$5+1):(\$6/1000) with linespoints pt 4 lc "dark-green" title 'QUIC (FEC-tunnel) p=0.1%', \
+              '' every 1::$offset+60:$block:$offset+89:$block using (\$5+1):(\$6/1000) with linespoints pt 8 lc "dark-green" title 'QUIC (FEC-tunnel) p=1.0%', \
+              '' every 1::$offset+90:$block:$offset+119:$block using (\$5+1):(\$6/1000) with linespoints pt 10 lc "dark-green" title 'QUIC (FEC-tunnel) p=5.0%'
 
 EOF
     done # $rate
